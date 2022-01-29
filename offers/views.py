cb739ef9b8063b59d6c939d2208cd07b4ab5e6cb
Bartek <barteklos1999@gmail.com>
2022-01-29 21:53:22 +0100

    finished delete option for offers

--- offers/views.py
+++ offers/views.py
@@ -2,8 +2,19 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Offer
 from .serializers import OfferSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Create your views here.
 class OfferViewSet(viewsets.ModelViewSet):
     queryset = Offer.objects.all()
     serializer_class = OfferSerializer
+
+    @action(detail=True, methods=['post'])
+    def delete(self,request,pk):
+        record = self.get_object()
+        if record.owner == request.user:
+            record.delete()
+            return Response(status=201)
+        else:
+            return Response(status=400)
